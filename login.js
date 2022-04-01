f17e87e3828eaa23775f5325002d3165a6635005
PizzaMan9000 <88388990+PizzaMan9000@users.noreply.github.com>
2022-04-01 19:34:34 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDfAdn8fhPp_SxjP5JGhBX2My9yQPpwcc4",
+    authDomain: "buzzin-a0686.firebaseapp.com",
+    databaseURL: "https://buzzin-a0686-default-rtdb.firebaseio.com",
+    projectId: "buzzin-a0686",
+    storageBucket: "buzzin-a0686.appspot.com",
+    messagingSenderId: "701370219789",
+    appId: "1:701370219789:web:b90d866e1bf618358d4444"
+  };
+  
+firebase.initializeApp(firebaseConfig);
